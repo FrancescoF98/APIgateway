65f6fb02f7d43585e2fee5df0f50a0ff6d90f34d
FrancescoF98 <f.fiocca@elitesoftwarehouse.com>
2025-06-13 17:25:10 +0200

    First commit

---
+++ src/main/java/com/elitesoftwarehouse/demo/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.elitesoftwarehouse.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/public/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults())  // enables default form login
+                .httpBasic(withDefaults()); // enables HTTP Basic authentication
+
+        return http.build();
+    }
+}
+
+
