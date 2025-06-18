1b6bf1467f02cff22044724592a58cb85cbfde0c
FrancescoF98 <f.fiocca@elitesoftwarehouse.com>
2025-06-18 11:08:52 +0200

    Fix: Il gateway non funzionava

--- src/main/java/com/elitesoftwarehouse/demo/config/SecurityConfig.java
+++ src/main/java/com/elitesoftwarehouse/demo/config/SecurityConfig.java
@@ -4,7 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -24,6 +26,22 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+//    @Bean
+//    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+//        return http
+//                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+//                .authorizeExchange(exchange -> exchange
+////                        .pathMatchers("/register", "/users").permitAll()
+//                        .pathMatchers("/admin/**").permitAll()
+//                        .anyExchange().authenticated()
+//                )
+//                .httpBasic()
+//                .and()
+//                .build();
+//    }
 }
+
+
 
 
